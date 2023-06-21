5259456e1cef4f888bf2f45ce4f6ff30bea6c1fe
Juanoliva06 <133877730+Juanoliva06@users.noreply.github.com>
2023-06-21 07:21:50 -0500

    Create codigo.py

---
+++ codigo.py
@@ -0,0 +1,12 @@
+idolo = input("¿Quién es tu ídolo? ")
+
+if idolo.lower() == "Messi":
+    print("¡Eso es genial! Cristiano Ronaldo es un increíble futbolista y una inspiración para muchos.")
+else:
+    print("Ese es un gran ídolo también, ¡pero para mí, Cristiano Ronaldo es realmente asombroso!")
+    
+    #Juan_Oliva
+    #Idolo
+    #IEM_ESCUELA_NORMAL_PASTO
+    #11-1
+
