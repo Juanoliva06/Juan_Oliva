0418f423eb2774c6012e592db945a1186c2dff7c
Juanoliva06 <133877730+Juanoliva06@users.noreply.github.com>
2023-06-21 08:18:04 -0500

    Create Sum_Python.py

---
+++ Sum_Python.py
@@ -0,0 +1,20 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+# Ejemplo de uso
+numero1 = 10
+numero2 = 5
+
+resultado_suma = suma(numero1, numero2)
+resultado_resta = resta(numero1, numero2)
+
+print("La suma de", numero1, "y", numero2, "es:", resultado_suma)
+print("La resta de", numero1, "y", numero2, "es:", resultado_resta)
+
+ #Yela
+    #Idolo
+    #IEM_ESCUELA_NORMAL_PASTO
+    #11-1
