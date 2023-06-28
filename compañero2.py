d279da7910bc426a75e3454c8ae66b7cd0263fbd
Juanoliva06 <133877730+Juanoliva06@users.noreply.github.com>
2023-06-28 08:06:03 -0500

    Update compañero2.py

--- compañero2.py
+++ compañero2.py
@@ -1,24 +1,18 @@
-def crear_biografia(nombre, fecha_nacimiento, lugar_nacimiento, descripcion):
-    biografia = f"Biografía de {nombre}\n\n"
-    biografia += f"Fecha de nacimiento: {fecha_nacimiento}\n"
-    biografia += f"Lugar de nacimiento: {lugar_nacimiento}\n\n"
-    biografia += f"Descripción: {descripcion}\n"
-    return biografia
-
-# Solicitar información al usuario
-nombre = input("Ingrese el nombre: ")
-fecha_nacimiento = input("Ingrese la fecha de nacimiento: ")
-lugar_nacimiento = input("Ingrese el lugar de nacimiento: ")
-descripcion = input("Ingrese una breve descripción: ")
-
-# Crear la biografía
-biografia = crear_biografia(nombre, fecha_nacimiento, lugar_nacimiento, descripcion)
-
-# Imprimir la biografía
-print("\n--- Biografía ---")
-print(biografia)
-
-    #Santiago_Enriquez
-    #Biografia
-    #IEM_ESCUELA_NORMAL_PASTO
-    #11-1
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Preguntas Varias</title>
+</head>
+<body>
+  <h1>Preguntas Varias</h1>
+  
+  <h2>Pregunta 1</h2>
+  <p>Respuesta 1</p>
+  
+  <h2>Pregunta 2</h2>
+  <p>Respuesta 2</p>
+  
+  <h2>Pregunta 3</h2>
+  <p>Respuesta 3</p>
+</body>
+</html>
