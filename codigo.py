c516798a7ee9c87ad06273938af4e59114b4c4e2
Juanoliva06 <133877730+Juanoliva06@users.noreply.github.com>
2023-06-28 08:10:44 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,9 +1,32 @@
-idolo = input("¿Quién es tu ídolo? ")
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mi Ídolo</title>
+</head>
+<body>
+  <h1>Conoce a mi ídolo</h1>
+  
+  <img src="imagen_idolo.jpg" alt="Mi Ídolo">
+  
+  <h2>Nombre: [Nombre de tu ídolo]</h2>
+  <p>Descripción: [Breve descripción de tu ídolo]</p>
+  
+  <h2>Logros:</h2>
+  <ul>
+    <li>[Logro 1]</li>
+    <li>[Logro 2]</li>
+    <li>[Logro 3]</li>
+  </ul>
+  
+  <h2>Redes Sociales:</h2>
+  <ul>
+    <li><a href="[Enlace a la cuenta de Twitter]">Twitter</a></li>
+    <li><a href="[Enlace a la cuenta de Instagram]">Instagram</a></li>
+    <li><a href="[Enlace a la página de Facebook]">Facebook</a></li>
+  </ul>
+</body>
+</html>
 
-if idolo.lower() == "Messi":
-    print("¡Eso es genial! Cristiano Ronaldo es un increíble futbolista y una inspiración para muchos.")
-else:
-    print("Ese es un gran ídolo también, ¡pero para mí, Cristiano Ronaldo es realmente asombroso!")
     
     #Juan_Oliva
     #Idolo
